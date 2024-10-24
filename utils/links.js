8579666cea90a382346ae4747d108672026a2ad0
T0nci <138928016+T0nci@users.noreply.github.com>
2024-10-24 19:08:56 +0200

    Add navbar partial and the links for it

---
+++ utils/links.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    text: "New message",
+    href: "/messages/create",
+  },
+  {
+    text: "Join",
+    href: "/join",
+  },
+  {
+    text: "Logout",
+    href: "/logout",
+  },
+];
