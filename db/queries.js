64661f1a88af503a7f6e8ed9d1075c92988c56ec
T0nci <138928016+T0nci@users.noreply.github.com>
2024-10-24 16:07:12 +0200

    Remove redundant query and variables

--- db/queries.js
+++ db/queries.js
@@ -1,22 +1,12 @@
 const db = require("./pool");
 
 class Messages {
-  #query;
-  constructor() {
-    this.#query = db.query;
-  }
-
   async getAllMessages() {
-    const { rows } = await this.#query("SELECT * FROM messages");
+    const { rows } = await db.query("SELECT * FROM messages");
     return rows;
   }
 }
 class Users {
-  #query;
-  constructor() {
-    this.#query = db.query;
-  }
-
   async createUser(username, password, firstName, lastName) {
     await db.query(
       "INSERT INTO users (username, password, first_name, last_name, role) VALUES($1, $2, $3, $4, 'none')",
@@ -26,18 +16,10 @@ class Users {
 
   async getUserByUsername(username) {
     const { rows } = await db.query(
-      "SELECT username FROM users WHERE username = $1",
+      "SELECT id, username, password FROM users WHERE username = $1",
       [username],
     );
 
-    return rows[0];
-  }
-
-  async getUserById(id) {
-    const { rows } = await this.#query(
-      "SELECT id, username, password FROM users WHERE id = $1",
-      [id],
-    );
     return rows[0];
   }
 }
